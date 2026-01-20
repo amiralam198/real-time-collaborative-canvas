f4d66a442755cb044efb583e494777fd31a82170
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2026-01-20 23:31:25 +0530

    updated server_url for deployment

--- client/config.js
+++ client/config.js
@@ -4,10 +4,10 @@
  */
 
 // Default: Server running on localhost:3000
-const SERVER_URL = 'http://localhost:3000';
+// const SERVER_URL = 'http://localhost:3000';
 
 // For production, update to your deployed server URL
-// const SERVER_URL = 'https://your-app.herokuapp.com';
+const SERVER_URL = 'https://real-time-collaborative-canvas-nine.vercel.app/';
 
 // For local network testing
 // const SERVER_URL = 'http://192.168.1.100:3000';
