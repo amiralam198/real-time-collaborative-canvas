c3216f0866693afdf6eeb7a67dd03561cce1a56d
amiralam198 <157512945+amiralam198@users.noreply.github.com>
2026-01-20 23:48:31 +0530

    update server_url

--- client/config.js
+++ client/config.js
@@ -7,7 +7,7 @@
 // const SERVER_URL = 'http://localhost:3000';
 
 // For production, update to your deployed server URL
-const SERVER_URL = 'https://real-time-collaborative-canvas-nine.vercel.app/';
+const SERVER_URL = 'https://real-time-collaborative-canvas-atn0.onrender.com/';
 
 // For local network testing
 // const SERVER_URL = 'http://192.168.1.100:3000';
